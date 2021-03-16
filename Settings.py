5ec129c1b17f89f232f6dc7b724b2d163557160b
Ilry <54525056+Ilry@users.noreply.github.com>
2021-03-16 21:44:36 +0800

    Create Settings.py

---
+++ Settings.py
@@ -0,0 +1,33 @@
+# coding=UTF-8
+# 江苏大学健康打卡网址
+url = 'https://pass.ujs.edu.cn/cas/login?service=http%3A%2F%2Fyun.ujs.edu.cn%2Fxxhgl%2Fyqsb%2Findex'
+
+# 发件邮箱设置项 （请填写个人信息）
+mail_host = "smtp.qq.com"#如果不是qq邮箱自行修改
+mail_user = ""
+mail_pass = ""#smtp授权码
+sender = ''
+
+
+# 百度 OCR 调用设置项 （请填写个人信息）
+APP_ID = ''
+API_KEY = ''
+SECRET_KEY = ''
+
+# 用户信息 （请填写个人信息）
+user_info = ['']
+
+username_info = {
+    '': '',
+}
+
+password_info = {
+    '': '',
+}
+
+receivers_info = {
+    '': '',
+}
+
+# 结果信息
+result_text = {}
